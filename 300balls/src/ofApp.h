e500d89e1da9d74d1acb4242c3a624262ead9287
Bruno Caldas Vianna <bruno@pobox.com>
2025-10-09 13:32:51 +0200

    300 balls

---
+++ 300balls/src/ofApp.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+		int posX[300], posY[300], velX[300], velY[300];
+		int diameter[300], gray[300];
+
+};

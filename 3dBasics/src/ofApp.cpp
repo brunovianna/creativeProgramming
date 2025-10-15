511bcd49574e648bfa83d941fdbe0c07906a6384
bruno caldas vianna <bruno@pobox.com>
2025-10-15 11:46:50 +0200

    3dBasics

---
+++ 3dBasics/src/ofApp.cpp
@@ -0,0 +1,105 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+void ofApp::setup(){
+    pointLight.setup();
+    pointLight.setPosition(-100, 200,300);
+	pointLight.enable();
+    ofEnableLighting();
+    ofEnableDepthTest();
+
+	// ofSetSmoothLighting(true);
+	// pointLight.setDiffuseColor( ofFloatColor(.85, .85, .55) );
+	// pointLight.setSpecularColor( ofFloatColor(1.f, 1.f, 1.f));
+
+    boxMaterial.setDiffuseColor(ofFloatColor::red);
+    boxMaterial.setShininess(0.02);
+
+
+
+}
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+    ofFill();
+    cam.begin();
+    ofSetColor(0,200,0);
+    ofDrawSphere(100,100,0,10.0f);
+    boxMaterial.begin();
+    ofTranslate(300,300);
+    box.draw();
+    boxMaterial.end();
+    cam.end();
+
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}

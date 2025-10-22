16658159b4f20b94de918bcc942135680d8b7cd3
bruno <bruno@pobox.com>
2025-10-22 12:37:23 +0200

    ball

--- ballObjects/src/ofApp.h
+++ ballObjects/src/ofApp.h
@@ -1,53 +1,16 @@
 #pragma once
-
 #include "ofMain.h"
 
-class Ball {
+class ofApp : public ofBaseApp {
+public:
+    void setup();
+    void update();
+    void draw();
 
-	public:
-		Ball();
-		Ball(int x, int y);
-		void display();
-		void update();
-		void applyForce (ofVec2f _force);
-
-	private:
-		ofVec2f location;
-		ofVec2f velocity;
-		ofVec2f acceleration;
-		ofColor colour;
-		float mass;
-
-
+    // physics
+    ofVec2f pos, vel, acc;
+    float mass;
+    float gravity;
+    float friction;
+    bool mouseHeld;
 };
-
-class ofApp : public ofBaseApp{
-
-	public:
-		void setup() override;
-		void update() override;
-		void draw() override;
-		void exit() override;
-
-		void keyPressed(int key) override;
-		void keyReleased(int key) override;
-		void mouseMoved(int x, int y ) override;
-		void mouseDragged(int x, int y, int button) override;
-		void mousePressed(int x, int y, int button) override;
-		void mouseReleased(int x, int y, int button) override;
-		void mouseScrolled(int x, int y, float scrollX, float scrollY) override;
-		void mouseEntered(int x, int y) override;
-		void mouseExited(int x, int y) override;
-		void windowResized(int w, int h) override;
-		void dragEvent(ofDragInfo dragInfo) override;
-		void gotMessage(ofMessage msg) override;
-		
-		Ball myBall;
-
-		vector <Ball> myBalls;
-
-		ofVec2f gravity;
-		ofVec2f wind;
-
-};
-

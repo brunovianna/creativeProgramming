72f7cc9b89f19f2f9ab7d19a5919654bbc190f96
bruno <bruno@pobox.com>
2025-12-01 16:53:51 +0100

    Peano L-system

---
+++ LSystem/src/Turtle.cpp
@@ -0,0 +1,38 @@
+#include "Turtle.h"
+
+void Turtle::setup(float _length, float _angle) {
+  length = _length;
+  angle = _angle;
+}
+
+void Turtle::render(string sentence) {
+  ofSetColor(0);
+  for (int i = 0; i < sentence.size(); ++i) {
+    char c = sentence[i];
+    if ((c == 'F' )||(c=='G')) {
+      //ofSetLineWidth(ofMap(i,10,1,0,sentence.size()*.1));
+      ofDrawLine(0, 0, 0, -length);
+      ofTranslate(0, -length);
+    } else if (c == '+') {
+      ofRotateRad(angle);
+    } else if (c == '-') {
+      ofRotateRad(-angle);
+    } else if (c == '[') {
+      ofPushMatrix();
+    } else if (c == ']') {
+      ofPopMatrix();
+    }
+    // if (c == 'F' || c == 'G') {
+    //   ofDrawLine(0, 0, 0, -length);
+    //   ofTranslate(0, -length);
+    // } else if (c == '+') {
+    //   ofRotateRad(angle);
+    // } else if (c == '-') {
+    //   ofRotateRad(-angle);
+    // } else if (c == '[') {
+    //   ofPushMatrix();
+    // } else if (c == ']') {
+    //   ofPopMatrix();
+    // }
+  }
+};
---
+++ LSystem/src/ofApp.cpp
@@ -0,0 +1,65 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+  //map<string, string> rules = {{"F", "FF+[+F-F-F]-[-F+F+F]"}};
+  map<string, string> rules = {{"X", "XFYFX+F+YFXFY-F-XFYFX"},{"Y", "YFXFY-F-XFYFX+F+YFXFY"}};
+  //map<string, string> rules = {{"F", "F-G+F+G-F"}, {"G", "G+F-G-F+G"}}; // Sierpinski Gasket variation
+
+  lsystem.setup("X", rules);
+  turtle.setup(10, ofDegToRad(90.0));
+
+  for (int i = 0; i < 4; ++i) {
+    lsystem.generate();
+  }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update() { ofBackground(255); }
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+   ofSetColor(0);
+  ofTranslate(ofGetWidth()*0.0, ofGetHeight()*1);
+  turtle.render(lsystem.sentence);
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
